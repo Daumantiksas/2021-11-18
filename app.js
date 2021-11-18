351cc0ba8aafac364160a0e42113c9667310b556
Mindaugas <mindkriauciunas@gmail.com>
2021-11-18 09:20:29 +0200

    pirmas

--- app.js
+++ app.js
@@ -3,17 +3,20 @@
         let body = document.querySelector('body')
         body.style.display= 'flex'
         body.style.flexWrap = 'wrap'
+        body.style.width = '840px'
+        body.style.margin = '0 auto'
+
         for(let i=0; i<16; i++) {
                 const newDiv = document.createElement("div");
                newDiv.style.backgroundColor = 'yellow'
                newDiv.style.width = '200px'
                newDiv.style.height = '200px'
-
                 newDiv.textContent= 'div';
                 body.appendChild(newDiv);
         }
 
 for (let div of document.querySelectorAll('div')){
-        div.onclick = (e) => e.target.style.backgroundColor = `rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)})`
+        div.onclick = (e) =>
+            e.target.style.backgroundColor = `rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)})`
 }
 
